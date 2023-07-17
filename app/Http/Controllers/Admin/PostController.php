568b469325b7d2ae6c89dc3f9896cd19bf5571e4
Valerio Billi <valerio.billi18@gmail.com>
2023-07-17 02:19:03 +0200

    first commit

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view("admin.posts.index", compact("posts"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("admin.posts.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostRequest $request)
+    {
+        $data = $request->validated();
+
+        $newPost = new Post();
+        $newPost->fill($data);
+        $newPost->save();
+
+        return to_route("admin.posts.show", $newPost);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view("admin.posts.show", compact("post"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view("admin.posts.edit", compact("post"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePostRequest  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        $data = $request->validated();
+
+        $post->title = $data['title'];
+        $post->content = $data['content'];
+        $post->image = $data['image'];
+      
+        $post->update();  
+        
+        return to_route("admin.posts.show", $post);
+   
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('admin.posts.index');
+    }
+}
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="container-fluid mt-5">
+    <div class="row justift-content-center">
+        <h1>Crea un nuovo post</h1>
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        <form action="{{ route("admin.posts.store") }}" method="POST" class="needs-validation">
+            @csrf
+
+            <label for="Title" class="mt-3">Titolo</label>
+            <input type="text" name="title" id="title" class="form-control">
+
+            <label for="content" class="mt-3">Contenuto</label>
+            <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+
+            <label for="image" class="mt-3">Indirizzo immagine</label>
+            <input type="text" name="image" id="image" class="form-control">
+
+            <input type="submit" class="form-control btn btn-primary mt-5" value="Crea nuovo post">
+
+        </form>
+
+    </div>
+     
+
+@endsection
